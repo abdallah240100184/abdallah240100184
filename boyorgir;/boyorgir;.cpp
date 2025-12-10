ed60fd2860ed70ae596354eb2b32385c0a8bcaaa
abdallah240100184 <abdallahsaad2007@gmail.com>
2025-12-10 04:45:22 +0200

    Add files via upload

---
+++ boyorgir;/boyorgir;.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    string username;
+    cin >> username;
+
+    set<char> distinctChars;
+
+    for (char c : username) {
+        distinctChars.insert(c);
+    }
+
+    if (distinctChars.size() % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else {
+        cout << "IGNORE HIM!" << endl;
+    }
+
+    return 0;
+}
+//Ai tool helped me to know the set<char function>
